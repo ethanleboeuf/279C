4f27150620c2882566f3ae8acf0d6ecc6227e5bb
ethanleboeuf <ethanl20@vt.edu>
2019-04-09 14:53:51 -0700

    Pushing changes

--- body_inertia_new.m
+++ body_inertia_new.m
@@ -1,6 +1,5 @@
 close all
 clear
-
 %% Constants
 % Base Section
 m_b = 1144.107; % mass of base
@@ -19,24 +18,24 @@ b_p = 1.85;
 h_p = 3.6;
 %% Inertia calcs
 % X-dim
-I_xb = (1/12)*m_b*(w_b)^2;
-I_xsp = (1/12)*m_sp*(w_sp)^2;
-d_xsp = 0.5*(w_b + w_sp);
-I_xp = (1/12)*m_p*(w_p)^2;
+I_xb = (1/12)*m_b*(w_b^2 + b_b^2);
+I_xsp = (1/12)*m_sp*(w_sp^2 + b_sp^2);
+d_xsp = 0.5*(w_b/2 + w_sp);
+I_xp = (1/12)*m_p*(w_p^2 + b_p^2);
 % Total X-dim
 I_x = I_xb + 2*(I_xsp + m_sp*(d_xsp^2)) + I_xp;
 % Y-dim
-I_yb = (1/12)*m_b*(b_b)^2;
-I_ysp = (1/12)*m_sp*(b_sp)^2;
-I_yp = (1/12)*m_p*(b_p)^2;
+I_yb = (1/12)*m_b*(b_b^2 + h_b^2);
+I_ysp = (1/12)*m_sp*(b_sp^2 + h_sp^2);
+I_yp = (1/12)*m_p*(b_p^2 + h_p^2);
 % Total Y-dim
-I_y = I_yb + 2*(I_ysp) + I_yp;
+I_y = (I_yb + m_b * (h_b/2)^2) + 2*(I_ysp+m_sp*(h_sp/2)^2) + (I_yp+m_p*(h_p/2+h_b)^2);
 % Z-dim
-I_zb = (1/12)*m_b*(h_b)^2;
+I_zb = (1/12)*m_b*(h_b^2 + w_b^2);
 d_zb = 0.5*(h_b);
-I_zsp = (1/12)*m_sp*(h_sp)^2;
-d_zsp = 0.5*(h_sp);
-I_zp = (1/12)*m_p*(h_p)^2;
+I_zsp = (1/12)*m_sp*(h_sp^2 + w_sp^2)^2;
+d_zsp = sqrt((0.5*(h_sp))^2 + (w_sp/2)^2);
+I_zp = (1/12)*m_p*(h_p^2 + w_p^2)^2;
 d_zp = h_b + 0.5*(h_p);
 % Total Z-dim
 I_z = (I_zb + m_b*(d_zb^2)) + 2*(I_zsp + m_sp*(d_zsp^2)) + (I_zp + m_p*(d_zp^2));
