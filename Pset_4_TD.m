6f61b06adf12119c1c2180a16d32cfa9e51726bc
ethanleboeuf <ethanl20@vt.edu>
2019-04-30 00:40:03 -0700

    Updates

--- Pset_4_TD.m
+++ Pset_4_TD.m
@@ -10,24 +10,54 @@ Ix_sat = I_vec(1);
 Iy_sat = I_vec(2);
 Iz_sat = I_vec(3);
 constants % access constants (mu, distance to earth, etc.)
-mu1 = 328900.56^-1; % From NASA, Barycenter GM
+
 omega = (sqrt((mu.earth + mu.moon + mu.sun)/dis.sun^3));
-r0 = [.98900883730044109; 0; 0.000802140914099732]; % initial position
-v0 = [0; 0.010697471710460349-3.24371e-3; 0]; % initial velocity
+mu1 = 328900.56^-1; % From NASA
+r0 = [.98900883730044109; 0; 0.000802140914099732]*dis.sun;
+v0 = [0; 0.010697471710460349-2.895683e-3; 0] * dis.sun * omega;
 w0 = [2; 1; 2.5] * pi()/180; % initial angular velocity
-DCM = eye(3);
+
+% DCM = eye(3);
+DCM =[.892539 .157379 -.422618; -.275451 0.932257 -.234570; .357073 0.325773 .875426];
 %% Gravity Gradient Torque
+
+% I made some changes in here. I included sun and earth positions in the
+% inertia out so we can get the exact gravity gradient for each of them
+% since mu1 is not actually the gravitational parameter and is instead a
+% mass ratio type thing
+
+
 sim('SOHO_sim_v1.slx')
 x = Inert_out(:,1);
 y = Inert_out(:,2);
 z = Inert_out(:,3);
-xyz =[x;y;z];
-% xyz_p = 
-R = sqrt((x.^2)+(y.^2)+(z.^2));
-cx = x./R;
-cy = y./R;
-cz = z./R;
-M = (3*mu1./(R.^3)).*[(Iz_sat - Iy_sat).*cy.*cz;(Ix_sat - Iz_sat).*cz.*cx;(Iy_sat - Ix_sat).*cx.*cy];
+
+x_s = Inert_out(:,4);
+y_s = Inert_out(:,5);
+z_s = Inert_out(:,6);
+
+x_e = Inert_out(:,7);
+y_e = Inert_out(:,8);
+z_e = Inert_out(:,9);
+
+xyz =[x y z];
+
+R_s = sqrt(((x - x_s).^2)+((y - y_s).^2)+((z - z_s).^2));
+R_e = sqrt(((x - x_e).^2)+((y - y_e).^2)+((z - z_e).^2));
+
+cx_s = x./R_s;
+cy_s = y./R_s;
+cz_s = z./R_s;
+
+cx_e = x./R_e;
+cy_e = y./R_e;
+cz_e = z./R_e;
+
+% M = (3*mu1./(R.^3)).*[(Iz_sat - Iy_sat).*cy.*cz;(Ix_sat - Iz_sat).*cz.*cx;(Iy_sat - Ix_sat).*cx.*cy];
+
+M_earth = (3*mu.earth./(R_e.^3)).*[((Iz_sat - Iy_sat).*cy_e.*cz_e)';...
+    ((Ix_sat - Iz_sat).*cz_e.*cx_e)';...
+    ((Iy_sat - Ix_sat).*cx_e.*cy_e)'];
 
 %% K coefficients of Moments of Inertia
 Ix_k = linspace(0,1e4);
