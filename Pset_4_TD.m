460f48bf8071a93e350b2ab4796cb4b93b0eb5be
Tunde Demuren <tdemuren15@gmail.com>
2019-04-30 13:00:00 -0700

    Update Pset_4_TD.m

--- Pset_4_TD.m
+++ Pset_4_TD.m
@@ -16,8 +16,6 @@ mu1 = 328900.56^-1; % From NASA
 r0 = [.98900883730044109; 0; 0.000802140914099732]*dis.sun;
 v0 = [0; 0.010697471710460349-2.895683e-3; 0] * dis.sun * omega;
 w0 = [2; 1; 2.5] * pi()/180; % initial angular velocity
-
-% DCM = eye(3);
 DCM =[.892539 .157379 -.422618; -.275451 0.932257 -.234570; .357073 0.325773 .875426];
 %% Gravity Gradient Torque
 
@@ -27,7 +25,8 @@ DCM =[.892539 .157379 -.422618; -.275451 0.932257 -.234570; .357073 0.325773 .87
 % mass ratio type thing
 
 
-sim('SOHO_sim_v1.slx')
+sim('SOHO_sim_v2.slx')
+size = length(Inert_out);
 x = Inert_out(:,1);
 y = Inert_out(:,2);
 z = Inert_out(:,3);
@@ -40,38 +39,41 @@ x_e = Inert_out(:,7);
 y_e = Inert_out(:,8);
 z_e = Inert_out(:,9);
 
-xyz =[x y z];
+xyz =[x,y,z];
+xyz_p = zeros(size,3);
+% Rotate inertial coord to principal axes frame
+[C] = out2mat(dcm_out);
+for i = 1:size
+   xyz_p(i,:) =  xyz(i,:)*C(:,:,i);
+end
+
+x_p = xyz_p(:,1);
+y_p = xyz_p(:,2);
+z_p = xyz_p(:,3);
 
 R_s = sqrt(((x - x_s).^2)+((y - y_s).^2)+((z - z_s).^2));
 R_e = sqrt(((x - x_e).^2)+((y - y_e).^2)+((z - z_e).^2));
 
-cx_s = x./R_s;
-cy_s = y./R_s;
-cz_s = z./R_s;
+cx_s = x_p./R_s;
+cy_s = y_p./R_s;
+cz_s = z_p./R_s;
 
-cx_e = x./R_e;
-cy_e = y./R_e;
-cz_e = z./R_e;
+cx_e = x_p./R_e;
+cy_e = y_p./R_e;
+cz_e = z_p./R_e;
 
-% M = (3*mu1./(R.^3)).*[(Iz_sat - Iy_sat).*cy.*cz;(Ix_sat - Iz_sat).*cz.*cx;(Iy_sat - Ix_sat).*cx.*cy];
-
-M_earth = (3*mu.earth./(R_e.^3)).*[((Iz_sat - Iy_sat).*cy_e.*cz_e)';...
-    ((Ix_sat - Iz_sat).*cz_e.*cx_e)';...
-    ((Iy_sat - Ix_sat).*cx_e.*cy_e)'];
-
-%% K coefficients of Moments of Inertia
-Ix_k = linspace(0,1e4);
-Iy_k = linspace(0,1e4);
-Iz_k = linspace(0,1e4);
-k_R = (Iz_k-Iy_k)/Ix_k;
-k_T = (Iz_k-Ix_k)/Iy_k;
-k_N = (Iy_k-Ix_k)/Iz_k;
-k_R_sat = (Iz_sat-Iy_sat)/Ix_sat;
-k_T_sat = (Iz_sat-Ix_sat)/Iy_sat;
-figure
-plot(k_T_sat,k_R_sat,'o')
-plot(k_T,k_R)
-hold on
-eqn1 = k_R.*k_T;
-eqn2 = 1 + 3.*k_T + k_R.*k_T;
+M_earth = zeros(size,3);
+M_sun = zeros(size,3);
+for ii = 1:size
+% Torque from earth 
+M_earth(ii,:) = (3*mu.earth./((R_e(ii)).^3)).*[((Iz_sat - Iy_sat).*cy_e(ii).*cz_e(ii))',...
+    ((Ix_sat - Iz_sat).*cz_e(ii).*cx_e(ii))',...
+    ((Iy_sat - Ix_sat).*cx_e(ii).*cy_e(ii))'];
+% Torque from sun
+M_sun(ii,:) = (3*mu.sun./((R_s(ii)).^3)).*[((Iz_sat - Iy_sat).*cy_s(ii).*cz_s(ii))',...
+    ((Ix_sat - Iz_sat).*cz_s(ii).*cx_s(ii))',...
+    ((Iy_sat - Ix_sat).*cx_s(ii).*cy_s(ii))'];
+end
+% Combined torque
+M = M_earth + M_sun;
 
