7d6fc149f60f86f4e1f828e0231aeb9ecfc1b516
ethanleboeuf <ethanl20@vt.edu>
2019-05-07 01:10:03 -0700

    Cleaning up (I think done)

--- Pset_4_TD.m
+++ Pset_4_TD.m
@@ -15,14 +15,11 @@ omega = (sqrt((mu.earth + mu.moon + mu.sun)/dis.sun^3));
 mu1 = 328900.56^-1; % From NASA
 r0 = [.98900883730044109; 0; 0.000802140914099732]*dis.sun;
 v0 = [0; 0.010697471710460349-2.895683e-3; 0] * dis.sun * omega;
-<<<<<<< HEAD
 % w0 = [2; 1; 2.5] * pi()/180; % initial angular velocity
-w0 = [0 ; 0; omega];
-=======
-% w0 = [.01; 2; .01] * pi()/180; % initial angular velocity
-w0 = [omega/100 ; omega;  omega/100];
->>>>>>> parent of ff3e13b... Woo! Nomimal and Perturbed now output
+% w0 = [0 ; 0; omega];
 
+w0 = [.01; 2; .01] * pi()/180; % initial angular velocity
+w0 = [omega/100 ; omega;  omega/100];
 % DCM =[.892539 .157379 -.422618; -.275451 0.932257 -.234570; .357073 0.325773 .875426];
 DCM = [0.999999671094767 0 -8.110550887097017e-04; 0 1 0; 8.110550887097016e-04 0 0.999999671094768];
 %% Gravity Gradient Torque
@@ -33,7 +30,7 @@ DCM = [0.999999671094767 0 -8.110550887097017e-04; 0 1 0; 8.110550887097016e-04 
 % mass ratio type thing
 
 
-sim('SOHO_sim_v3.slx')
+sim('SOHO_sim_v4.slx')
 size = length(Inert_pos_out);
 x = Inert_pos_out(:,1);
 y = Inert_pos_out(:,2);
--- post_sim_plots.m
+++ post_sim_plots.m
@@ -1,11 +1,8 @@
 %% Plotting Results from Sim
 close all
 n = length(t);
-<<<<<<< HEAD
-R = zeros(3, n);
-T = zeros(3, n);
-N = zeros(3, n);
-=======
+
+
 % R = zeros(3, n);
 % T = zeros(3, n);
 % N = zeros(3, n);
@@ -15,11 +12,11 @@ N = zeros(3, n);
 % N(:, :) = RTN_out(:, 3, :);
 % specific_triad_plot(R, T , N, 'RTN')
 % 
-[A] = out2mat(dcm_out(:, 1:9));
-w_out = dcm_out(:, 10:12)';
-ang_momentum_I_plot(w_out, sc, A)
-ang_velocity_I_plot(w_out, A, sc)
-axes_plot(A, sc)
+% [A] = out2mat(dcm_out(:, 1:9));
+% w_out = dcm_out(:, 10:12)';
+% ang_momentum_I_plot(w_out, sc, A)
+% ang_velocity_I_plot(w_out, A, sc)
+% axes_plot(A, sc)
 % 
 % figure(2)
 % unit_sun = zeros(3, n);
@@ -51,22 +48,43 @@ xlabel('Time, days')
 ylabel('Gravity Gradient Torque, Nm')
 title('Gravity Gradient Torque over one Orbit around L1')
 hold off
->>>>>>> parent of ff3e13b... Woo! Nomimal and Perturbed now output
 
-R(:, :) = RTN_out(:, 1, :);
-T(:, :) = RTN_out(:, 2, :);
-N(:, :) = RTN_out(:, 3, :);
-specific_triad_plot(R, T , N, 'RTN')
 
-[A] = out2mat(dcm_out(:, 1:9));
-axes_plot(A, sc)
+% [A] = out2mat(dcm_out(:, 1:9));
+% axes_plot(A, sc)
+% 
+% figure()
+% scatter3(Inert_pos_out(1:10:end,1), Inert_pos_out(1:10:end,2), Inert_pos_out(1:10:end,3), '.', 'b')
+% hold on 
+% scatter3(Inert_pos_out(1:10:end,4), Inert_pos_out(1:10:end,5), Inert_pos_out(1:10:end,6), '.', 'r')
+% scatter3(Inert_pos_out(1:10:end,7), Inert_pos_out(1:10:end,8), Inert_pos_out(1:10:end,9), '.', 'g')
+% xlabel('I')
+% ylabel('J')
+% zlabel('K')
+% hold off
 
-figure()
-scatter3(Inert_pos_out(1:10:end,1), Inert_pos_out(1:10:end,2), Inert_pos_out(1:10:end,3), '.', 'b')
-hold on 
-scatter3(Inert_pos_out(1:10:end,4), Inert_pos_out(1:10:end,5), Inert_pos_out(1:10:end,6), '.', 'r')
-scatter3(Inert_pos_out(1:10:end,7), Inert_pos_out(1:10:end,8), Inert_pos_out(1:10:end,9), '.', 'g')
-xlabel('I')
-ylabel('J')
-zlabel('K')
+
+
+subplot(3,1,1)
+plot(t/3600/24, M(:, 1), 'k', 'LineWidth', 3)
+hold on
+xlabel('time, days')
+ylabel('M_x, Nm')
+grid on
+hold off
+
+subplot(3,1,2)
+plot(t/3600/24, M(:, 2), 'k', 'LineWidth', 3)
+hold on
+xlabel('time, days')
+ylabel('M_y, Nm')
+grid on
+hold off
+
+subplot(3,1,3)
+plot(t/3600/24, M(:, 3), 'k', 'LineWidth', 3)
+hold on
+xlabel('time, days')
+ylabel('M_z, Nm')
+grid on
 hold off
